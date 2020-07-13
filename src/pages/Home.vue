8bb44d84fefda0ac7fd667e75e4f619690a8ef77
Barranka <cat123_93@mail.ru>
2020-07-13 18:39:18 +0300

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1>This is test Home page. Go to <router-link class="link" to="/shop">Shop page</router-link></h1>
+      </div>
+    </section>
+  </div>
+</template>
